755e986279b1d429757c3913d30d69d70214b85f
monsemorab <monserratmorab@gmail.com>
2017-11-26 18:45:39 -0300

    inicio de proyecto
    se agregan los archivos iniciales para el proyecto

---
+++ src/test/java/com/mantenimiento/equipomedico/EquipoMedicoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mantenimiento.equipomedico;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EquipoMedicoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
