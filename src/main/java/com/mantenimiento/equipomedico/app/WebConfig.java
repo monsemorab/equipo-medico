62374c271321aeeae8034e54ce3d7fce5f3f859c
Brenda Quinhonez <brenda@softwarenatura.com>
2020-03-20 18:29:57 -0300

    Configuración de CORS
    Signed-off-by: Brenda Quinhonez <brenda@softwarenatura.com>

--- src/main/java/com/mantenimiento/equipomedico/app/WebConfig.java
+++ src/main/java/com/mantenimiento/equipomedico/app/WebConfig.java
@@ -1,9 +1,12 @@
 package com.mantenimiento.equipomedico.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -17,4 +20,15 @@ public class WebConfig extends WebSecurityConfigurerAdapter
             authorizeRequests().
             antMatchers("/api/**").permitAll();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
+    }
+
 }
