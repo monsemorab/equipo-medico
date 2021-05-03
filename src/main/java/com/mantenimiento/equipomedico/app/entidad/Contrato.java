a2e3b412e93abefc6c2b2f834bd7851f9f5db72b
Brenda <bren.rom.luj@gmail.com>
2021-05-03 18:33:34 -0400

    Se sacó el id autogenerado en contratos
    Signed-off-by: Brenda <bren.rom.luj@gmail.com>

--- src/main/java/com/mantenimiento/equipomedico/app/entidad/Contrato.java
+++ src/main/java/com/mantenimiento/equipomedico/app/entidad/Contrato.java
@@ -20,7 +20,6 @@ public class Contrato implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
     @Column(name = "id")
     private Long id;
 
