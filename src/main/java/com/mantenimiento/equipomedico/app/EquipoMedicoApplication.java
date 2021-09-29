cb5897e65947e3bca1090c631cd499a07f93765f
Brenda <bren.rom.luj@gmail.com>
2021-09-28 23:09:07 -0400

    Se agrega annotation para indicar la carpeta base
    Signed-off-by: Brenda <bren.rom.luj@gmail.com>

--- src/main/java/com/mantenimiento/equipomedico/app/EquipoMedicoApplication.java
+++ src/main/java/com/mantenimiento/equipomedico/app/EquipoMedicoApplication.java
@@ -2,8 +2,10 @@ package com.mantenimiento.equipomedico.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.mantenimiento.equipomedico.app"})
 public class EquipoMedicoApplication {
 
 	public static void main(String[] args) {
